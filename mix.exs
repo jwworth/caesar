cd3cae1f82895ad326ed531b4cc21cf21949bf74
Jake Worth <jake@jakeworth.com>
2018-01-30 16:00:43 -0600

    Autoformat mix.exs

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule Caesar.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :caesar,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :caesar,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
